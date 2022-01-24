51b0d5a111b4ef1788070a80857dfad7403e2d20
Brian S <brians@Brians-MacBook-Air.local>
2022-01-24 13:08:38 -0500

    Solved twoSum

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -1,15 +1,15 @@
-const nums = [9, 1,17,20,5, 4];
+const nums = [3, 1,17,20,5, 4];
 target = 6;
 
 let twoSum = function(nums, target) {
-    let total = 0;
-    for (i=0 ;i < nums.length; i++){
-        if(nums[i] + nums[i+1] == target) {
-            console.log(nums[i] + "+" + nums[i+1] + "=" + target)
-        } if (nums[0] + nums[i] == target){
-            console.log(nums[0] + "+" + nums[i] + "=" + target)
-        } 
+    for (let i = 0; i < nums.length; i++){
+        for (let j = i + 1; j < nums.length; j++){
+            if (nums[j] + nums[i] == target){ 
+                console.log(nums[j] + "+" + nums[i] + "=" + target)
+            }
+        }
+    
     }
 
-}
+} 
 twoSum(nums, target)
