96c01e9040cc636c51cbb3cd2b8827cc79234316
Brian S <brians@Brians-MacBook-Air.local>
2022-02-02 09:45:13 -0500

    .

--- hw-day12.js
+++ hw-day12.js
@@ -1,7 +1,17 @@
 //Given a sentence reverse order: "the sky is blue" -> "blue is sky the". 
 let string = "the sky is blue"
 const reverseWords = str => str.trim().split(/\s+/).reverse().join(' ')
+//trim is saying if there is space in the beginning or end trim it
+//the center of split is the spaces or another way of saying space
+//you use s.split(" ") so it would look like //["the","sky","is","blue"]
+let arrs = string.split(" ")
+let reverseArr = arrs.reverse()
+let res = reverseArr.join(" ")
+//console.log(reverseArray.join(" ")) couldve solved it this way to
+//console.log(res) -> this also solves it as well
+//console.log(...reverseArr)// you can do it this way finish with it this way
 console.log(reverseWords(string))
+
 
 //2. You are given 2 arrays, one called list and one called cart. 
 //Create a new version of the list where the items in the cart have been removed. 
@@ -13,6 +23,21 @@ for (let i = 0; i < cart.length; i++){
     list.push(cart[i]);
 }
 console.log(list)
+
+const newShoppingList = list.filter((item) => !cart.includes(item));
+
+console.log(newShoppingList)
+
+// second solution 
+
+let newList = []
+for(let item of list) {
+    if(!cart.includes(item)) {
+        newList.push(item)
+    }
+}
+console.log(newList)
+//solve the problem with a method for this one^^^
 
 //3.  Write a function that given an array of numbers, find the largest number and console.log it
 
